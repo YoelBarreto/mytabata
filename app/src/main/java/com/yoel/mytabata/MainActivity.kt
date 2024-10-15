e10c2600e85f7678bdb2588f41f0dc2c364c13e9
Yoel <yobelb8@gmail.com>
2024-10-15 12:50:36 +0100

    Archivos modificados + boton añadido.

--- app/src/main/java/com/yoel/mytabata/MainActivity.kt
+++ app/src/main/java/com/yoel/mytabata/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.yoel.mytabata.ui.theme.MytabataTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MytabataTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Counter(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +32,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MytabataTheme {
-        Greeting("Android")
+fun Counter(modifier: Modifier = Modifier) {
+    Column (modifier = Modifier){
+        Text(
+            text = "00",
+            modifier = Modifier
+        )
+        Button(onClick = {}){
+            Text(
+                text = "Pulsar"
+            )
+        }
     }
 }
